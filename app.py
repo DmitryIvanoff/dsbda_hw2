299afa8bcca9230d13603f382eab3f92bddbd23e
dmitry_iv <d-iv.96@yandex.ru>
2018-12-25 20:19:11 +0300

    significant modification

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from pyspark import SparkContext, SparkConf
 import sys
 import re
 import cassandra.cluster as cascl
-
+from cassandra import ConsistencyLevel
+from tempfile import NamedTemporaryFile
+import cassandra
 
 def LineMap(line):
     '''
@@ -15,10 +17,21 @@ def LineMap(line):
     hour = int((res[1].split(' '))[-1])
     return (pri*100+hour,1)
     
-def SaveToDB(CollectedRdd,KeyspaceName='syslog',TableName='statistics'):
+def SaveToDB(CollectedRdd,cluster,KeyspaceName='syslog',TableName='statistics'):
     '''
     '''
-    cluster = cascl.Cluster()
+    session = cluster.connect(KeyspaceName)
+    record = session.prepare("INSERT INTO {} (hour, priority, amount) VALUES (?, ?, ?)".format(TableName))
+    batch = cassandra.query.BatchStatement(consistency_level=ConsistencyLevel.QUORUM)
+    for (key,value) in CollectedRdd:
+        (p,h)=divmod(key,100) #key = priority*100+hour
+        batch.add(record, (h,p,value))
+    session.execute(batch)
+    session.shutdown()
+
+def CreateKeySpaceAndTable(cluster,KeyspaceName='syslog',TableName='statistics'):
+    '''
+    '''
     session = cluster.connect()
     session.execute("CREATE KEYSPACE IF NOT EXISTS %s WITH REPLICATION \
                     = { 'class' : 'SimpleStrategy','replication_factor' : 1 }" % (KeyspaceName,))
@@ -26,19 +39,15 @@ def SaveToDB(CollectedRdd,KeyspaceName='syslog',TableName='statistics'):
     session.execute("CREATE TABLE IF NOT EXISTS {} \
                     (hour tinyint, priority tinyint, amount int, \
                     PRIMARY KEY (hour,priority))".format(TableName)) 
-    for (key,value) in CollectedRdd:
-        (p,h)=divmod(key,100) #key = priority*100+hour
-        session.execute("INSERT INTO {} (hour, priority, amount) VALUES (%s, %s, %s)"
-                       .format(TableName),(h,p,value))   
-    cluster.shutdown()
+    session.shutdown()
 
-def printFromDb(file=sys.stdout,KeyspaceName='syslog',TableName='statistics'):
+
+def printFromDb(cluster,KeyspaceName='syslog',TableName='statistics',file=sys.stdout):
     '''
     '''
-    cluster = cascl.Cluster()
     session = cluster.connect(KeyspaceName)
     rows = session.execute("SELECT hour, priority, amount FROM {}".format(TableName))
-    cluster.shutdown()
+    session.shutdown()
     p = ['emerg','alert','crit','error','warning','notice','info','debug']
     res = []
     for row in rows:
@@ -46,16 +55,25 @@ def printFromDb(file=sys.stdout,KeyspaceName='syslog',TableName='statistics'):
         res.append((row.hour,row.priority,row.amount))
     return res
 
-def SparkCalculate(filename):
+def SparkCalculate(context,filename,tmpFile):
     '''
     '''
-    conf = SparkConf().setAppName('CountingSyslogsByHours')
-    sc = SparkContext(conf=conf)
-    rdd = sc.textFile(filename) #create rdd from log file which contains 
+    rdd = context.textFile(filename) #create rdd from log file which contains 
                                    #lines like: "%PRI%: %timegenerated% %HOSTNAME%
                                    #%syslogtag%%msg:::drop-last-lf%\n"
-    result = rdd.map(LineMap).sortByKey().reduceByKey(lambda a,b: a+b).collect()
-    sc.stop()
+    rdd = rdd.map(LineMap).sortByKey().reduceByKey(lambda a,b: a+b) # logline->(key,1)->sort->(key,amount)
+    if rdd.isEmpty():
+        return None   #TODO raise Exeption
+    oldrdd = context.pickleFile(tmpFile.name)
+    if oldrdd.isEmpty():
+        newrdd = rdd
+    else:
+        newrdd = oldrdd.union(rdd).sortByKey().reduceByKey(lambda a,b: a+b) # oldrdd U rdd->sort->(key,amount)
+    tmpFile = NamedTemporaryFile(delete=True)
+    tmpFile.close()
+    newrdd.saveAsPickleFile(tmpFile.name)
+    open(filename, "w")  #remove all logs from logfile
+    result = newrdd.collect()
     return result
 
 
@@ -63,10 +81,23 @@ if __name__ == '__main__':
     if len(sys.argv) !=2:
         print ('Usage: app.py <logfile>')
         sys.exit(-1)
-    statistics = SparkCalculate(sys.argv[1])
+
+    KeyspaceName = 'syslog'
+    TableName = 'statistics'
+    cluster = cascl.Cluster()
+    CreateKeySpaceAndTable(cluster,KeyspaceName,TableName)
+    conf = SparkConf().setAppName('CountingSyslogsByHours')
+    sc = SparkContext(conf=conf)
+    tmpFile = NamedTemporaryFile(delete=True)
+    tmpFile.close()
+    sc.emptyRDD().saveAsPickleFile(tmpFile.name)
+    statistics = SparkCalculate(sc,sys.argv[1],tmpFile=tmpFile)
     #writing to Cassandra
-    SaveToDB(statistics)
-    printFromDb()
+    SaveToDB(statistics,cluster)
+    #printing from Cassandra
+    printFromDb(cluster)
+    cluster.shutdown()
+    sc.stop()
 
 
     
--- app_test.py
+++ app_test.py
@@ -2,31 +2,52 @@ import unittest
 import app
 import sys
 from pyspark import SparkContext, SparkConf
+import cassandra.cluster as cascl
+
+
 
 
 class TestMethods(unittest.TestCase):
 
+  conf = SparkConf().setAppName('TestCountingSyslogsByHours')
+  sc = SparkContext(conf=conf)
+  cluster = cascl.Cluster()
+  KeyspaceName = 'test'
+  TableName = 'statistics'
+  tmpFile = app.NamedTemporaryFile(delete=True)
+  tmpFile.close()
+
+  def setUp(self):
+    app.CreateKeySpaceAndTable(cluster,KeyspaceName,TableName)
+    sc.emptyRDD().saveAsPickleFile(tmpFile.name)
+    tmpFile.close()
+
   def test_LineMap(self):
 
-      s = '77: Dec 23 23:16:38 divine0ff-Aspire-E1-570G anacron[1024]: Normal exit (1 job run)'
-      print(sys.argv)
-      self.assertEqual(app.LineMap(s), (523,1))
+    s = '77: Dec 23 23:16:38 divine0ff-Aspire-E1-570G anacron[1024]: Normal exit (1 job run)'
+    print(sys.argv)
+    self.assertEqual(app.LineMap(s), (523,1))
 
   def test_spark(self):
 
-      res = app.SparkCalculate('logfile.test')
-      print('test computedRdd:',res)
-      self.assertCountEqual(res, [(606,1),(511,2),(611,2)])
+    res = app.SparkCalculate(sc,'logfile.test',tmpFile)
+    print('test computedRdd:',res)
+    self.assertCountEqual(res, [(606,1),(511,2),(611,2)])
 
   def test_saveToDB(self):
 
-      app.SaveToDB([(606,1),(511,2),(611,2)],KeyspaceName='test')
-      res=app.printFromDb(file=None,KeyspaceName='test')
-      print('test saved table:',res)
-      self.assertCountEqual(res, [(6,6,1),(11,6,2),(11,5,2)])
+    app.SaveToDB([(606,1),(511,2),(611,2)],cluster,KeyspaceName=KeyspaceName,TableName=TableName)
+    res=app.printFromDb(cluster,file=None,KeyspaceName=KeyspaceName,TableName=TableName)
+    print('test saved table:',res)
+    self.assertCountEqual(res, [(6,6,1),(11,6,2),(11,5,2)])
 
+def tearDown(self):
+
+    cluster.shutdown()
+    sc.stop()
 
 if __name__ == '__main__':
+    
     with open('logfile.test','w') as f:
         f.write("30: Dec 24 06:05:18 divine0ff-Aspire-E1-570G systemd[1]: Mounting /boot/efi...\n\
 46: Dec 25 11:54:49 divine0ff-Aspire-E1-570G rsyslogd:  bla bla...\n\
